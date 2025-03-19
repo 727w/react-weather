1a4bcdb82d0dc9e15c5baa6a29b5fcc6612be3f9
727w <{ID}+{username}@users.noreply.github.com165373911+727w@users.noreply.github.com>
2025-03-19 09:43:58 +0700

    adding default parameter

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,35 @@
 import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   const [input, setInput] = useState("");
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [location, setLocation] = useState("");
+  const [location, setLocation] = useState("jakarta");
   const API_KEY = "NXTAFBRP54Z4P5BSJNHRZ4DSJ";
 
   useEffect(() => {
     if (!location) return;
 
-    async function fetchData() {
+    async function fetchData(location) {
       try {
-        const data = await fetch(
+        const response = await fetch(
           `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/?key=${API_KEY}`,
           { mode: "cors" }
         );
-        const result = await data.json();
+        const result = await response.json();
         setData(result);
+        setLoading(false);
+        console.log(result);
       } catch (error) {
         console.error(error);
-      } finally {
         setLoading(false);
       }
     }
 
-    fetchData();
+    fetchData(location);
   }, [location]);
 
   return (
@@ -36,6 +39,15 @@ function App() {
         onChange={setInput}
         onSearch={() => setLocation(input)}
       />
+      {loading ? (
+        <FontAwesomeIcon icon={faSpinner} spinPulse size="2xl" />
+      ) : (
+        <div className="text-white">
+          <h1 className="text-4xl font-bold">{data.resolvedAddress}</h1>
+          <h2 className="text-2xl">{data.currentConditions.icon}</h2>
+          <h3 className="text-xl">{data.currentConditions.temp}</h3>
+        </div>
+      )}
     </div>
   );
 }
